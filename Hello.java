4ea42eadcc1f14d2cbf0a9e85df294b3fed74f47
plober <plober@gmail.com>
2015-09-13 12:49:23 -0300

    Test of the first HelloWOrld (which I thought I had sent previously)

---
+++ Hello.java
@@ -0,0 +1,10 @@
+/*
+ *
+ * My First (actually, n+1, where n>=10 IIRC) program in java
+ *
+ */
+public class Hello {   // 3 attempts until I was forced to read the docs and saved as "Hello.java"
+   public static void main(String[] args) {
+      System.out.println("Hello, world!"); 
+   }
+}
